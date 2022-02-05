c6ac21955a74f2ff454e383b5eedc1b450ee3f84
Sumit <akoliyasumit@hotmail.com>
2022-02-05 22:48:13 +0530

    2nd: Simple npm server

--- server.js
+++ server.js
@@ -1,7 +1,40 @@
+const fs = require('fs');
 const http = require('http')
 
-const server = http.createServer(()=>{
-    console.log('request made');
+const server = http.createServer((req, res)=>{
+    //console.log(req.url,req.method); // -> / GET
+    res.setHeader('Content-type', 'text/html')
+    //res.write('<p>Hello</p>')
+    let path =`./views/`;
+    switch(req.url){
+        case `/`:
+            path+= `index.html`
+            res.statusCode=200
+            break
+        case `/about`:
+            path+= `about.html`
+            res.statusCode=200
+            break
+        case `/about-me`:
+            res.statusCode=301
+            res.setHeader('Location',`/about`)
+            res.end()
+        default :
+            path+= `404.html`
+            res.statusCode=404
+            break
+    }
+    fs.readFile(path,(err,data)=>{
+        if(err){
+            console.log('err', err)
+            res.end()
+        }
+        else{
+            //res.write(data)
+            res.statusCode=200
+            res.end(data)
+        }
+    })
 })
 
 server.listen(3000,()=>{
