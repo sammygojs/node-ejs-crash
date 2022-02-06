15ac8fd1bbe70d8f1dc8521ae9f8ce93e012bf33
Sumit <akoliyasumit@hotmail.com>
2022-02-06 15:43:37 +0530

    simple views with ejs

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 const express = require('express')
 const app = express()
 
+<<<<<<< HEAD
 app.listen(3000)
 
 app.get('/',(req,res)=>{
@@ -9,6 +10,21 @@ app.get('/',(req,res)=>{
 
 app.get('/about',(req,res)=>{
     res.sendFile('./views/about.html', {root: __dirname})
+=======
+//register view engine
+app.set('view engine','ejs');
+//custom setting of folder if not views
+//app.set('views','myviews')
+
+app.listen(3000)
+
+app.get('/',(req,res)=>{
+    res.render('index')
+})
+
+app.get('/about',(req,res)=>{
+    res.render('about')
+>>>>>>> f1ca6f0 (simple views with ejs)
 })
 
 app.get('/about-me',(req,res)=>{
@@ -16,6 +32,10 @@ app.get('/about-me',(req,res)=>{
 })
 
 app.use((req,res)=>{
+<<<<<<< HEAD
     res.sendFile('./views/404.html', {root: __dirname})
+=======
+    res.status(404).render('404')
+>>>>>>> f1ca6f0 (simple views with ejs)
 })
 
