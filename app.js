4026929904aed923e7b45434acbdb518d3addaf5
Sumit <akoliyasumit@hotmail.com>
2022-02-06 18:27:22 +0530

    server with partials and middleware

--- app.js
+++ app.js
@@ -8,19 +8,43 @@ app.set('view engine','ejs');
 
 app.listen(3000)
 
-app.get('/',(req,res)=>{
-    res.render('index')
+app.use((req, res, next)=>{
+    console.log("new request made")
+    console.log('host: ', req.hostname)
+    console.log('path: ', req.path)
+    console.log('method: ', req.method)
+    next()
 })
+
+app.use((req, res, next)=>{
+    console.log("next middleware called")
+    next()
+})
+
+//static files and middleware
+//http://localhost:3000/styles.css can be used to access
+app.use(express.static('public'))
+
+app.get('/',(req,res)=>{
+    const blogs = [
+        {title: 'Sumit finds a job', snippet:'Lorem ipsum dolor sit amet consectetur adipisicing elit.'},
+        {title: 'Rumit finds a job', snippet:'Lorem ipsum dolor sit amet consectetur adipisicing elit.'},
+        {title: 'Zumit finds a job', snippet:'Lorem ipsum dolor sit amet consectetur adipisicing elit.'},
+    ]
+    res.render('index', { title:'Home' , blogs})
+})
+
+
 
 app.get('/about',(req,res)=>{
-    res.render('about')
+    res.render('about' , { title:'About' })
 })
 
-app.get('/about-me',(req,res)=>{
-    res.redirect('/about')
+app.get('/blogs/create',(req,res)=>{
+    res.render('create', { title:'Create Blog' })
 })
 
 app.use((req,res)=>{
-    res.status(404).render('404')
+    res.status(404).render('404', { title:'Not found' })
 })
 
